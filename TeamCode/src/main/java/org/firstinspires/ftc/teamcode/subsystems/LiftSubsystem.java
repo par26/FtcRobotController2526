a8907164c94dd89094fab6b7c1fe9a2f13f59b95
samxielaptop <crimsoncrympty@gmail.com>
2026-01-02 19:18:17 -0800

    LiftSubsystem.java implemented

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.seattlesolvers.solverslib.hardware.motors.Motor;
+import com.seattlesolvers.solverslib.hardware.motors.MotorGroup;
+
+public class LiftSubsystem extends SubsystemBase {
+
+    private MotorGroup m_lift;
+    private Motor m_left;
+    private Motor m_right;
+
+    public static int ARISE_TICK_COUNT = 1200;
+    public static int RESET_TICK_COUNT = 0;
+    public static double kP = 0.5;
+    private int targetTickCount;
+
+
+    public static int TOLERANCE = 15;
+
+    public LiftSubsystem(HardwareMap hwMap) {
+        m_left = new Motor(hwMap, "leftLiftMotor");
+        m_right = new Motor(hwMap, "rightLiftMotor");
+
+        m_lift = new MotorGroup(m_left, m_right);
+        m_lift.setRunMode(Motor.RunMode.PositionControl);
+        m_lift.setPositionTolerance(TOLERANCE);
+        m_lift.setPositionCoefficient(kP);
+    }
+
+    public void arise() {
+        m_lift.setTargetPosition(ARISE_TICK_COUNT);
+    }
+
+    public void reset() {
+        m_lift.setTargetPosition(RESET_TICK_COUNT);
+    }
+
+    @Override
+    public void periodic() {
+        m_lift.set(1);
+    }
+}
